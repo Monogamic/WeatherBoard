4321684ba5640c3e2d3962f5d001012d90f942aa
Serhii <imonogamic@gmail.com>
2020-04-08 13:29:52 +0300

    ---> jQwery + prob with changes elem

---
+++ src/scripts/index.js
@@ -0,0 +1,31 @@
+var ex1 = $('#searchField');
+ex1.attr('placeholder', 'Rename');
+
+var ex2 = $('#weather-img');
+ex2.width(400);
+ex2.height(400);
+
+var ex3 = $('#container-bg');
+$('#container-bg').css('background-color', 'red');
+
+var ex4 = $('#weather-img');
+$('#weather-img').attr('src', '/src/assets/images/ex4.jpg');
+
+$('.hidden').hide();
+
+
+
+// var parent = $('.variable');
+// var child = $('.value');
+
+// function changer (first, second) {
+//     for (i=0; i<first.length; i++) {
+//         first[i].after(second[i]);
+//     }
+// }
+
+// changer (child, parent);
+
+	
+// $('.variable').after($('.value'));
+// $('.value').before($('.variable'));
